693a2f6de9042fb5754132f5902984de74c09721
200275643 <chandrareddy.mec@gmail.com>
2016-10-04 23:13:34 -0400

    Added About Me page

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+/*GET About Me Page */
+router.get('/About Me', function(req, res, next) {
+  res.render('About Me', { title: 'About Me' });
+});
+
 
 module.exports = router;
